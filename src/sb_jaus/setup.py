ca9fb2c62ad7db007163cb17a55c77f91da08421
Gareth Ellis <gareth.ellis0@gmail.com>
2017-06-03 14:44:09 -0400

    update jaus node to work with library

--- src/sb_jaus/setup.py
+++ src/sb_jaus/setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-#packages=['sb_jaus'],
-#package_dir={''}
+    packages=['format'],
+    package_dir={'': 'src/jaus'}
 )
 
 setup(**d)
